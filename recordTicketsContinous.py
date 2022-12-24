0988369834f29e005d68e859ea07b1a14a5269b4
sezgink <sezginkutlu00@gmail.com>
2022-12-24 23:07:38 +0300

    New chrome options

--- recordTicketsContinous.py
+++ recordTicketsContinous.py
@@ -26,9 +26,15 @@ currentStartLocation = "Eskişehir"
 currentTerminationLocation = "İstanbul(Söğütlü Ç.)"
 
 options = webdriver.ChromeOptions()
+options.add_argument('start-maximized')
+options.add_argument('enable-automation')
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--ignore-ssl-errors')
 options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-browser-side-navigation')
+options.add_argument('--disable-gpu')
 
 driver = webdriver.Chrome(chrome_driver_adress,chrome_options=options)
 
