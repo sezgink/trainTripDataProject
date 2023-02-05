2a33a0541930c33b8ed8c718aafdc32134a208e0
sezgink <sezginkutlu00@gmail.com>
2023-02-06 00:28:30 +0300

    data fetcher

---
+++ dataFetcher.py
@@ -0,0 +1,31 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# def getDBConnection():
+#     _dbConnection = mysql.connector.connect(
+#         host=os.environ.get("sqlhost"),
+#         user=os.environ.get("sqluser"),
+#         password=os.environ.get("sqlpass"),
+#         database="train_data"
+#     )
+#     return _dbConnection
+
+dbConnection = mysql.connector.connect(
+  host=os.environ.get("sqlhost"),
+  user=os.environ.get("sqluser"),
+  password=os.environ.get("sqlpass"),
+  database="train_data"
+)
+cursor= dbConnection.cursor()
+
+def GetTripsBetweenDataDates(dataDateFrom,dataDateTo):
+    query = f"select * from trips where data_date>={dataDateFrom} and dataDate<={dataDateTo}"
+    cursor.execute(query)
+    tripsBetween = cursor.fetchall()
+    return tripsBetween
+
+_trips = GetTripsBetweenDataDates("2023-01-11 19:10:00","2023-02-15 19:10:00")
+print(_trips)
+    # addQuery = "INSERT INTO trips (trip_date, data_date, trip_direction, empty_economy, empty_business) VALUES (%s, %s, %s, %s,%s)"
