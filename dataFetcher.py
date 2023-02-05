d24748a8d00ade1bf6663c6dfce51c77a6487c63
sezgink <sezginkutlu00@gmail.com>
2023-02-06 00:58:23 +0300

    New fetch queries

--- dataFetcher.py
+++ dataFetcher.py
@@ -21,11 +21,24 @@ dbConnection = mysql.connector.connect(
 cursor= dbConnection.cursor()
 
 def GetTripsBetweenDataDates(dataDateFrom,dataDateTo):
-    query = f'select * from trips where (data_date>"{dataDateFrom}" and data_date<"{dataDateTo}");'
+    query = f'select * from trips where (data_date>="{dataDateFrom}" and data_date<="{dataDateTo}");'
     cursor.execute(query)
     tripsBetween = cursor.fetchall()
     return tripsBetween
 
-_trips = GetTripsBetweenDataDates("2023-01-11 19:10:00","2023-01-12 19:10:00")
+def GetTripsBetweenTripDates(tripDateFrom,tripDateTo):
+    query = f'select * from trips where (trip_date>="{tripDateFrom}" and trip_date<="{tripDateTo}");'
+    cursor.execute(query)
+    tripsBetween = cursor.fetchall()
+    return tripsBetween
+
+def GetTripsBetweenTripDateAndDataDate(tripDateInterval,dataDateInterval):
+    query = f'select * from trips where (trip_date>="{tripDateInterval[0]}" and trip_date<="{tripDateInterval[1]}" and \
+        data_date>="{dataDateInterval[0]}" and data_date<="{dataDateInterval[1]}");'
+    cursor.execute(query)
+    tripsBetween = cursor.fetchall()
+    return tripsBetween
+
+_trips = GetTripsBetweenTripDateAndDataDate(["2023-01-11 19:10:00","2023-01-11 22:10:00"],["2023-01-9 19:10:00","2023-01-11 22:10:00"])
 print(_trips)
     # addQuery = "INSERT INTO trips (trip_date, data_date, trip_direction, empty_economy, empty_business) VALUES (%s, %s, %s, %s,%s)"
