7ecac25d3e1b80cb4386775cc51ebcf929bc6156
sezgink <sezginkutlu00@gmail.com>
2023-02-06 01:05:23 +0300

    Empty economy query

--- dataFetcher.py
+++ dataFetcher.py
@@ -39,6 +39,13 @@ def GetTripsBetweenTripDateAndDataDate(tripDateInterval,dataDateInterval):
     tripsBetween = cursor.fetchall()
     return tripsBetween
 
+def GetEmptyEconomyBetweenTripDateAndDataDate(tripDateInterval,dataDateInterval):
+    query = f'select trip_date,data_date,empty_economy from trips where (trip_date>="{tripDateInterval[0]}" and trip_date<="{tripDateInterval[1]}" and \
+        data_date>="{dataDateInterval[0]}" and data_date<="{dataDateInterval[1]}");'
+    cursor.execute(query)
+    tripsBetween = cursor.fetchall()
+    return tripsBetween
+
 _trips = GetTripsBetweenTripDateAndDataDate(["2023-01-11 19:10:00","2023-01-11 22:10:00"],["2023-01-9 19:10:00","2023-01-11 22:10:00"])
 print(_trips)
     # addQuery = "INSERT INTO trips (trip_date, data_date, trip_direction, empty_economy, empty_business) VALUES (%s, %s, %s, %s,%s)"
